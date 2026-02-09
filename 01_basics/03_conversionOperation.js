2acd87fb47aa693f4ceb5532cd355f12f579003a
Rajan Singh <Rajangnd315@gmail.com>
2026-02-09 13:01:14 +0000

    conversionOperation of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "unknown"
+//console.log( typeof score);
+//console.log(typeof(score));
+
+let valueInnumber = Number(score);
+//console.log(typeof valueInnumber);
+//console.log(valueInnumber);
+
+//"33" => 33
+//"33abd" => nan
+//"true" => 1; & false => 0
+
+let isLoggedIn = 1
+let boolenIsLoggedIn = Boolean(isLoggedIn);
+console.log(boolenIsLoggedIn);
+
+// 0 => flase : & 1 => true
+// "name" => true : & "" => false
+//"" => false
+
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
