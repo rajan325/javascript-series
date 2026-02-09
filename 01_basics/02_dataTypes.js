b2991e120cbd14da02a30e3be23dc7a96e9bfa62
Rajan Singh <Rajangnd315@gmail.com>
2026-02-09 12:11:27 +0000

    js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict" // treat all js code as modern version
+// alert(3+3) // we can use nodejs , not browser js code 
+console.log("hello raj")
+console.log(3 + 3)
+
+// primitive data types in js :-
+// number => 2 to power 53
+// BigInt
+// String => ""
+// Boolean => true/false
+// null => satanlonevalue
+// undefined =>
+//     Symbol => unique
+
+
+//object 
+console.log(typeof 123)
+console.log(typeof "raj")
+console.log(typeof null); // object
+console.log (typeof true); //boolean
+console.log(typeof undefined);// undefined
